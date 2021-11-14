da229845d2f28eb21c7bf2e09adb2d667967689d
Lars Kecker <lars.kecker2009@gmail.com>
2021-11-14 18:06:31 +0100

    feat: toDecimal exception handling

--- src/main/java/me/kecker/jsonparser/NumberUtils.java
+++ src/main/java/me/kecker/jsonparser/NumberUtils.java
@@ -1,8 +1,13 @@
 package me.kecker.jsonparser;
 
+import me.kecker.jsonparser.exceptions.IllegalNumberException;
+
 public class NumberUtils {
 
-    public static int toDecimal(char character) {
+    public static int toDecimal(char character) throws IllegalNumberException {
+        if (character < '0' || character > '9') {
+            throw new IllegalNumberException("Character '" + character + "' is not a decimal digit.");
+        }
         return character - '0';
     }
 
--- src/test/java/me/kecker/jsonparser/NumberUtilsTest.java
+++ src/test/java/me/kecker/jsonparser/NumberUtilsTest.java
@@ -1,16 +1,27 @@
 package me.kecker.jsonparser;
 
+import me.kecker.jsonparser.exceptions.IllegalNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NumberUtilsTest {
 
     @Test
     @DisplayName("toDecimal() should return the decimal value")
-    void testToDecimal() {
+    void testToDecimal() throws IllegalNumberException {
         int value = NumberUtils.toDecimal('0');
         assertThat(value).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("toDecimal() should throw exception for non-numeric characters")
+    @ValueSource(chars = {'%', 'A'})
+    void testToDecimalInvalidValues(char input) {
+        assertThrows(IllegalNumberException.class, () -> NumberUtils.toDecimal(input));
     }
 }
