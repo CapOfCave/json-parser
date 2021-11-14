a3e082fd3a98684e2691532076c96bbdb4ce7062
Lars Kecker <lars.kecker2009@gmail.com>
2021-11-14 12:01:48 +0100

    feat: state advancement

--- src/main/java/me/kecker/JsonParseState.java
+++ src/main/java/me/kecker/JsonParseState.java
@@ -1,13 +1,23 @@
 package me.kecker;
 
 public class JsonParseState {
+    private String source;
     private char current;
+    private int currentIndex;
 
-    public JsonParseState(String abcd) {
-        this.current = abcd.charAt(0);
+
+    public JsonParseState(String source) {
+        this.source = source;
+        this.currentIndex = 0;
+        this.current = source.charAt(0);
     }
 
     public char current() {
         return current;
+    }
+
+    public void advance() {
+        this.currentIndex++;
+        this.current = this.source.charAt(this.currentIndex);
     }
 }
--- src/test/java/me/kecker/JsonParseStateTest.java
+++ src/test/java/me/kecker/JsonParseStateTest.java
@@ -10,10 +10,18 @@ class JsonParseStateTest {
 
     @Test
     @DisplayName("Newly created object looks at first char")
-    public void testAdvance() {
+    public void testCurrent() {
         JsonParseState parserState = new JsonParseState("ABCD");
         assertThat(parserState.current()).isEqualTo('A');
 
     }
 
+    @Test
+    @DisplayName("Advance should make the next character current")
+    public void testAdvance() {
+        JsonParseState parserState = new JsonParseState("ABCD");
+        parserState.advance();
+        assertThat(parserState.current()).isEqualTo('B');
+
+    }
 }
