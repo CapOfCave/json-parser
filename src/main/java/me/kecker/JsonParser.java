06f9f84ae50d90a098d6fbdbd212712122c995a4
Lars Kecker <lars.kecker2009@gmail.com>
2021-11-14 12:15:44 +0100

    feat: implemented parseBoolean

--- src/main/java/me/kecker/JsonParser.java
+++ src/main/java/me/kecker/JsonParser.java
@@ -2,6 +2,12 @@ package me.kecker;
 
 public class JsonParser {
     public static boolean parseBoolean(String source) {
+        if (source.equals("true")) {
+            return true;
+        }
+        if (source.equals("false")) {
+            return false;
+        }
         return true;
     }
 }
--- src/test/java/me/kecker/JsonParserTest.java
+++ src/test/java/me/kecker/JsonParserTest.java
@@ -9,11 +9,17 @@ class JsonParserTest {
 
 
     @Test
-    @DisplayName("parseBoolean should return true for true values")
-    void testParseBoolean() {
+    @DisplayName("parseBoolean should return true for input 'true'")
+    void testParseBooleanTrue() {
         boolean result = JsonParser.parseBoolean("true");
         assertThat(result).isEqualTo(true);
     }
 
+    @Test
+    @DisplayName("parseBoolean should return false for input 'false'")
+    void testParseBooleanFalse() {
+        boolean result = JsonParser.parseBoolean("false");
+        assertThat(result).isEqualTo(false);
+    }
 
 }
