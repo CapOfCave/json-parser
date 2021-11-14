284dfe5dc789ed1e2aeea8f5db9b01e8b129dafd
Lars Kecker <lars.kecker2009@gmail.com>
2021-11-14 21:24:02 +0100

    feat: add stateless json parse method

--- src/main/java/me/kecker/jsonparser/JsonParser.java
+++ src/main/java/me/kecker/jsonparser/JsonParser.java
@@ -1,35 +1,14 @@
 package me.kecker.jsonparser;
 
+import me.kecker.jsonparser.exceptions.JsonParseException;
+
 import java.util.Collections;
 import java.util.Map;
 
 public class JsonParser {
 
-    @Deprecated
-    public static Object parse(String source) {
-        if (source.startsWith("\"")) {
-            return parseString(source);
-        }
-        return parseBoolean(source);
-    }
-
-    @Deprecated
-    public static boolean parseBoolean(String source) {
-        if (source.equals("true")) {
-            return true;
-        }
-        if (source.equals("false")) {
-            return false;
-        }
-        throw new IllegalArgumentException("Input '" + source + "' is not a valid boolean.");
-    }
-
-    @Deprecated
-    public static String parseString(String source) {
-        return source.substring(1, source.length() - 1);
-    }
-
-    public static Map<String, Object> parseObject(String source) {
-        return Collections.emptyMap();
+    public static Object parse(String source) throws JsonParseException {
+        JsonParseState jsonParseState = new JsonParseState(source);
+        return jsonParseState.json();
     }
 }
--- src/test/java/me/kecker/jsonparser/JsonParserTest.java
+++ src/test/java/me/kecker/jsonparser/JsonParserTest.java
@@ -1,5 +1,7 @@
 package me.kecker.jsonparser;
 
+import me.kecker.jsonparser.exceptions.JsonParseException;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,30 +10,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JsonParserTest {
 
     @Test
-    @DisplayName("parse should return Boolean value for input 'true'")
-    void testParseForInputTrue() {
-        Object result = JsonParser.parse("true");
+    @DisplayName("parse should return json value")
+    void testParseForInputTrue() throws JsonParseException {
+        Object result = JsonParser.parse(" [ \"someElement\" ] ");
         assertThat(result)
-                .isInstanceOf(Boolean.class)
-                .isEqualTo(Boolean.TRUE);
-    }
-
-    @Test
-    @DisplayName("parse should return Boolean value for input 'false'")
-    void testParseForInputFalse() {
-        Object result = JsonParser.parse("false");
-        assertThat(result)
-                .isInstanceOf(Boolean.class)
-                .isEqualTo(Boolean.FALSE);
-    }
-
-    @Test
-    @DisplayName("parse should return String value for quoted string")
-    void testParseForQuotedStringInput() {
-        Object result = JsonParser.parse("\"string\"");
-        assertThat(result)
-                .isInstanceOf(String.class)
-                .isEqualTo("string");
+                .asInstanceOf(InstanceOfAssertFactories.ARRAY)
+                .hasSize(1);
+        Object[] array = (Object[]) result;
+        assertThat(array[0]).isEqualTo("someElement");
     }
 
 }
